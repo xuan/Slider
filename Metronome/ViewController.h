e984d5854a49dac4dd48732956424cd15faaed97
Xuan Nguyen <darth.xuan@gmail.com>
2013-02-11 15:29:28 -0700

    Initial Commit

---
+++ Metronome/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Metronome
+//
+//  Created by Xuan Nguyen on 2/11/13.
+//  Copyright (c) 2013 Xuan Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Metronome/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Metronome
+//
+//  Created by Xuan Nguyen on 2/11/13.
+//  Copyright (c) 2013 Xuan Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
